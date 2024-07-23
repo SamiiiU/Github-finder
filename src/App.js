f16fe1421e6df31e2d4ff8b91f0c12cbd2a9253f
sami Um <sammiiiullah@gmail.com>
2024-07-23 19:15:24 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className='min-h-screen bg-black'>
+      <div className='container text-gray-2 00 py-3'>
+        
+      </div>
     </div>
   );
 }

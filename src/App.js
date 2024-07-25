6c6fab8760babc479d02fee4ba541c06278b0f99
sami Um <sammiiiullah@gmail.com>
2024-07-25 15:20:40 +0500

    add routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Logo_comp from './components/Logo_comp';
+import User from './Routes/User';
 
 function App() {
   return (
     
     <div className='min-h-screen bg-black'>
-      <div className='container text-gray-100 py-3'>
+      <div className='container py-3 text-gray-100'>
         <Logo_comp />
+        <Routes>
+          <Route path='/' element = {<User/>}></Route>
+        </Routes>
       </div>
     </div>
   );
--- src/components/Logo_comp.js
+++ src/components/Logo_comp.js
@@ -3,10 +3,12 @@ import React from 'react';
 
 const Logo_comp = () => {
   return (
-    <div>
+    <div className='flex pb-2 gap-x-4 justify-center items-center border-b border-gray-500'>
       <img src='/Logos/github-mark-white.png' className='w-20 py-3'/>
-        Github
+        
+      <h1 className='font-bold text-5xl'>Github User</h1>  
     </div>
+
   )
 }
 
